af8d277c38f35e8f8cb59d76228986ebb66d5b8f
suhong <67825309+tnghd5761@users.noreply.github.com>
2021-02-02 22:58:49 +0900

    Feb02_practice
    create component using concat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import TOC from "./components/TOC";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
 import Subject from "./components/Subject";
 import Control from "./components/Control";
 import './App.css';
@@ -8,8 +9,9 @@ import './App.css';
 class App extends Component {
   constructor(props){
     super(props);
+    this.max_content_id = 3;
     this.state = {
-      mode:'read',
+      mode:'create',
       selected_content_id:2,
       subject:{title:'WEB', sub:'World Wide Web!'}, 
       welcome:{title:'Welcome', desc:'Hello, React!!'},
@@ -22,10 +24,11 @@ class App extends Component {
   }
   render(){
     console.log('App render');
-    var _title, _desc = null;
+    var _title, _desc, _article = null;
     if(this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if(this.state.mode ==='read'){
       var i = 0;
       while(i < this.state.contents.length){
@@ -36,7 +39,18 @@ class App extends Component {
           break;
         }
         i = i + 1;
-      }  
+      }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if(this.state.mode === 'create'){
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        this.max_content_id = this.max_content_id + 1;
+        var _contents = this.state.contents.concat(
+          {id:this.max_content_id, title:_title, desc:_desc}
+        )
+        this.setState({
+          contents:_contents
+        });
+      }.bind(this)}></CreateContent>
     }
     return (
       <div className="App">
@@ -60,7 +74,7 @@ class App extends Component {
             mode:_mode
           });
         }.bind(this)}></Control>
-        <Content title={_title} desc={_desc}></Content> 
+        {_article}
       </div>
     );
   }
